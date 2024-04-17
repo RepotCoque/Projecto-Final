f3ea0a98fafce5569a7e717635906ebec38a3534
RepotCoque <93662583+RepotCoque@users.noreply.github.com>
2024-04-17 10:32:38 +0200

    2.0

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const baseURL = 'https://api.example.com/characters'; // Reemplaza con la URL de la API de personajes
+const baseURL = 'https://rickandmortyapi.com/api/character'; // Reemplaza con la URL de la API de personajes
 
 async function searchCharacters() {
     const searchTerm = document.getElementById('searchInput').value.trim();
@@ -11,26 +11,4 @@ async function searchCharacters() {
     } catch (error) {
         console.error('Error al buscar personajes:', error);
     }
-}
-
-function displayCharacters(characters) {
-    const characterList = document.getElementById('characterList');
-    characterList.innerHTML = ''; // Limpiar la lista antes de mostrar nuevos resultados
-
-    if (characters.length === 0) {
-        characterList.innerHTML = '<p>No se encontraron personajes.</p>';
-        return;
-    }
-
-    characters.forEach(character => {
-        const characterCard = `
-            <div class="character">
-                <h2>${character.name}</h2>
-                <p><strong>Edad:</strong> ${character.age}</p>
-                <p><strong>Género:</strong> ${character.gender}</p>
-                <p><strong>Descripción:</strong> ${character.description}</p>
-            </div>
-        `;
-        characterList.innerHTML += characterCard;
-    });
 }
